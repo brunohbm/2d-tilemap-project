bde7e4d88284fe85e11f8535b0e5969ac8d17382
Bruno Henrique <brunohbm23@gmail.com>
2018-08-22 22:58:07 -0300

    city's tilemap

---
+++ Assets/Scripts/PlayerMoviment.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoviment : MonoBehaviour {
+
+    public float speed;
+
+	void Update () {
+        Move();
+	}
+
+    void Move() {
+        Vector3 moviment = GetAxis() * speed;
+        transform.Translate(moviment * Time.deltaTime);
+    }
+
+    Vector3 GetAxis() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        return new Vector3(moveHorizontal, moveVertical, 0);
+    }
+}
