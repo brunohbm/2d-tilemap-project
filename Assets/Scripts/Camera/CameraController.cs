191ca6adf127cfaef864ef3620706ef1ac1d0150
Bruno Henrique <brunohbm23@gmail.com>
2018-08-27 19:28:07 -0300

    Camera Moviment

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform player;
+    public Vector3 offset;
+    public float smoothSpeed;
+	
+	void FixedUpdate () {
+        Vector3 desiredPosition = player.position + offset;
+        Vector3 smothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smothedPosition;
+	}
+}
